5a3dc27a596d286e90abf2f445f166c37f27c079
Caerus <snake7726@gmail.com>
2023-10-26 15:08:59 +0800

    Use the "crate"

---
+++ src/basic/guessing_game_2.rs
@@ -0,0 +1,21 @@
+use rand::Rng;
+use std::io;
+
+fn create_random_num() -> u32 {
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    random_number
+}
+
+pub fn guessing_game_by_random_number() {
+    let random_num = create_random_num();
+
+    println!("Please input your guess number");
+
+    let mut guess_num = String::new();
+
+    io::stdin().read_line(&mut guess_num)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess_num);
+    println!("Secret number: {random_num}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod basic {
     pub mod guessing_game;
     pub mod guessing_game_1;
+    pub mod guessing_game_2;
 }
 
 use basic::guessing_game;
 use basic::guessing_game_1;
+use basic::guessing_game_2;
 
 fn main() {
-    guessing_game::guessing_game_simple();
+    // guessing_game::guessing_game_simple();
+    // guessing_game_1::variable_test();
+    // guessing_game_1::placeholder_test();
+    guessing_game_2::guessing_game_by_random_number();
 }
 
