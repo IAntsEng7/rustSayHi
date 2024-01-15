957c6ab84739354f9c2e32944e8eadebcd9f9ca0
Caerus <snake7726@gmail.com>
2024-01-15 11:32:22 +0800

    practice::fibonacci_sequence;

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@ mod basic {
 }
 
 mod common_concepts {
-    pub mod variable;
+    pub mod control_flow;
     pub mod data_type;
     pub mod function;
-    pub mod control_flow;
+    pub mod variable;
 }
 
 mod practice {
     pub mod convert_temperatures;
+    pub mod fibonacci_sequence;
 }
 
 use basic::guessing_game;
@@ -24,14 +25,14 @@ use basic::guessing_game_2;
 use basic::guessing_game_3;
 use basic::guessing_game_4;
 use basic::guessing_game_5;
-use common_concepts::variable;
+use common_concepts::control_flow;
 use common_concepts::data_type;
 use common_concepts::function;
-use common_concepts::control_flow;
+use common_concepts::variable;
 use practice::convert_temperatures;
+use practice::fibonacci_sequence;
 
 fn main() {
-
     // Intro
     // guessing_game::guessing_game_simple();
     // guessing_game_1::variable_test();
@@ -40,7 +41,6 @@ fn main() {
     // guessing_game_3::guessing_game_and_check();
     // guessing_game_4::guessing_game_until_correct();
     // guessing_game_5::guessing_game_compare_by_match();
-
 
     // common_concepts
     // variable::immutable_test();
@@ -62,16 +62,23 @@ fn main() {
 
     // practice
     // practice::convert_temperatures
-    let fahrenheit_temperature = 98.6;
-    let celsius_temperature = convert_temperatures::convert_temperature(fahrenheit_temperature, "fahrenheit", "celsius");
-
-    match celsius_temperature {
-        Some(result) => {
-            println!("{} Fahrenheit is equal to {} Celsius", fahrenheit_temperature, result);
-        }
-        None => {
-            println!("Invalid unit conversion");
-        }
-    }
+    // let fahrenheit_temperature = 98.6;
+    // let celsius_temperature =
+    //     convert_temperatures::convert_temperature(fahrenheit_temperature, "fahrenheit", "celsius");
+    //
+    // match celsius_temperature {
+    //     Some(result) => {
+    //         println!(
+    //             "{} Fahrenheit is equal to {} Celsius",
+    //             fahrenheit_temperature, result
+    //         );
+    //     }
+    //     None => {
+    //         println!("Invalid unit conversion");
+    //     }
+    // }
+    // practice::fibonacci_sequence
+    let n_th = 93;
+    let result = fibonacci_sequence::fibonacci_at_n(n_th);
+    println!("The {}th Fibonacci number is: {}", n_th, result)
 }
-
